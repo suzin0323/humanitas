d1014a70265e5903e300a775d6f8320695ef7490
suzin0323 <sujinryu0323@gmail.com>
2025-11-12 17:48:53 +0900

    Delete src directory

--- src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "Humanitas", description: "Reading test" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ko">
-      <body style={{ margin:0, background:"#F8F5F0", color:"#222" }}>{children}</body>
-    </html>
-  );
-}
